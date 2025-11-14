fe8bb9809ac2c928dc36bf0a6be4e9710db1a596
Julien Moutinho <julm+ngipkgs@sourcephile.fr>
2025-11-14 01:29:59 +0100

    feat(manuals): init options rendering

--- manuals/default.nix
+++ manuals/default.nix
@@ -7,6 +7,7 @@
   manuals,
   modulesPath,
   nixos-modules,
+  nixosOptionsDoc,
   perl,
   pkgs,
   python3,
@@ -55,6 +56,7 @@ let
 
     patchPhase = ''
       runHook prePatch
+      ln -sf ${manuals.optionsMyST} manuals/options.md
       magick ${finalAttrs.passthru.logo} manuals/_static/_img/nix.pdf
       substituteInPlace manuals/index.md \
         --replace-fail '@MANUALS_VERSION@' "${version}"
@@ -146,4 +148,120 @@ lib.recurseIntoAttrs {
       runHook postInstall
     '';
   });
+
+  # Description: generates a valid MyST syntax from `manuals.optionsDoc.optionsNix`.
+  #
+  # Explanation: NixOS' `nixos-render-docs options commonmark`
+  # generates a markdown that is not compatible with MyST,
+  # and assumes it's only used in the context of NixOS.
+  optionsMyST =
+    let
+      fixContent =
+        text:
+        lib.replaceStrings
+          [
+            "(#opt-"
+            "{.note}"
+            "{.warning}"
+            "{var}"
+          ]
+          [
+            "(https://nixos.org/manual/nixos/unstable/#opt-"
+            "{note}"
+            "{warning}"
+            "{option}"
+          ]
+          (lib.removeSuffix "\n" text);
+      renderCode =
+        arg:
+        if lib.typeOf arg == "string" then
+          fixContent arg
+        else
+          let
+            text = fixContent arg.text;
+          in
+          if arg._type == "literalExpression" then
+            if lib.match ".*\n.*" text == null then
+              ''`${lib.replaceStrings [ "`" ] [ "\\`" ] text}`''
+            else
+              ''
+
+                ```
+                ${lib.replaceStrings [ "```" ] [ "\\`\\`\\`" ] text}
+                ```
+              ''
+          else if arg._type == "literalMD" then
+            text
+          else
+            # FixMe(completeness): unsupported arg._type
+            assert (lib.trace arg._type false);
+            "";
+      renderId = lib.concatMapStringsSep "-" (
+        part:
+        lib.concatStrings (
+          lib.map (
+            c: if "0" <= c && c <= "9" || "a" <= c && c <= "z" || "A" <= c && c <= "Z" then c else "_"
+          ) (lib.stringToCharacters part)
+        )
+      );
+    in
+    writeText "options.md" (
+      ''
+        {#User_What_is_the_tree_of_options}
+        # What is the tree of options?
+
+        The following tree of options is provided by importing:
+        - `inputs.NGIpkgs.nixosModules.programs`
+        - `inputs.NGIpkgs.nixosModules.services`
+
+      ''
+      + lib.concatStringsSep "\n\n" (
+        lib.concatAttrValues (
+          lib.mapAttrs (optName: opt: [
+            (
+              ''
+                {#option-${renderId opt.loc}}
+                ## ${lib.escapeXML optName}
+              ''
+              + lib.optionalString opt.readOnly ''
+                - ReadOnly: ${toString opt.readOnly}
+              ''
+              + ''
+                - Type: `${opt.type}`
+                - Description: ${renderCode opt.description}
+              ''
+              + lib.optionalString (opt ? "default") ''
+                - Default: ${renderCode opt.default}
+              ''
+              + lib.optionalString (opt ? "example") ''
+                - Example: ${renderCode opt.example}
+              ''
+            )
+          ]) manuals.optionsDoc.optionsNix
+        )
+      )
+    );
+
+  optionsDoc = nixosOptionsDoc {
+    revision = version;
+    options =
+      lib.flip lib.removeAttrs [ "_module" ]
+        (lib.evalModules {
+          class = "nixos";
+          specialArgs = {
+            inherit pkgs modulesPath;
+          };
+          modules = [
+            {
+              config = {
+                # Explanation: do not check anything
+                # because NixOS options are not included.
+                # See also comment in NixOS' `noCheckForDocsModule`.
+                _module.check = false;
+              };
+              imports = lib.attrValues nixos-modules.programs ++ lib.attrValues nixos-modules.services;
+            }
+          ];
+        }).options;
+  };
 }
--- manuals/index.md
+++ manuals/index.md
@@ -1,2 +1,9 @@
 # NGIpkgs — Nix packages and services for softwares supported by the Next Generation Internet
 Version: @MANUALS_VERSION@
+
+```{toctree}
+:maxdepth: 1
+:hidden: true
+
+options.md
+```
